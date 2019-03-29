6b55cec3c19c8707ac91b2925bc74e7f8105f987
Chris Roman <Christopherroman85@gmail.com>
2019-03-29 13:17:23 -0500

    server is listening and created file structure

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost/budget';
+const mongoDbUri = process.env.MONGO_URI || connectionString
+
+mongoose.connect(mongoDbUri, {useNewUrlParser: true});
+
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose connected at', connectionString);
+});
+
+mongoose.connection.on('diconnected', () => {
+	console.log('Mongoose disconnected ');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log('Mongoose error ', err);
+})
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+const session = require('express-session');
+
+const User = require('./models/user');
+
+// const userController = require('./controllers/user');
+
+require('./db/db')
+
+app.use(session({
+	secret: 'super secret string',
+	resave: 'false',
+	saveUninitialized: false
+}));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+// app.use('/users', userController);
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log('Server is listening on port ' + PORT);
+})
