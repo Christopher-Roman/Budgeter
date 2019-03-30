ac20281e3a620cd51bac7941e650bdb63754a305
Chris Roman <Christopherroman85@gmail.com>
2019-03-30 15:21:33 -0500

    Login route is working

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,18 +5,40 @@ const bcrypt = require('bcrypt')
 // Register Post Route
 
 router.post('/register', async (req, res) => {
-		const password = req.body.password;
-		const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-		console.log(passwordHash);
-		const userEntry = {};
-		userEntry.username = req.body.username;
-		userEntry.password = passwordHash;
-		const user = await User.create(userEntry);
-		console.log(user);
-		req.session.username = req.body.username;
-		req.session.logged = true;
-		req.session.message = undefined;
-		res.redirect('/');
+	const password = req.body.password;
+	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+	const userEntry = {};
+	userEntry.username = req.body.username;
+	userEntry.password = passwordHash;
+	const user = await User.create(userEntry);
+	req.session.username = req.body.username;
+	req.session.logged = true;
+	req.session.message = undefined;
+	res.redirect('/');
+});
+
+// Login Post Route
+
+router.post('/login', async (req, res) => {
+	try {
+		const foundUser = await User.findOne({username: req.body.username});
+		if(foundUser) {
+			if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+				req.session.username = req.body.username;
+				req.session.logged = true;
+				req.session.message = undefined;
+				res.redirect('/');
+			} else {
+				req.session.message = 'Username or password are incorrect.'
+				res.redirect('/users/login')
+			}
+		} else {
+			req.session.message = 'Username or password are incorrect.'
+			res.redirect('/users/login')
+		}
+	} catch(err) {
+		res.send(err);
+	}
 })
 
 
