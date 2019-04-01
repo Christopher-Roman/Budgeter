902413a256599708f0267256e49b2de9e5916094
Chris Roman <Christopherroman85@gmail.com>
2019-04-01 14:41:32 -0500

    Budget delete route is working

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -32,19 +32,27 @@ router.post('/new', async (req, res) => {
 })
 
 // Budget Delete Route
-router.delete('/:index', async (req, res) => {
+router.delete('/:id', async (req, res) => {
 	try {
-		const deletedBudget = await Budget.findByIdAndDelete(req.params.index);
-		let deletedBudgetItemsIds = [];
-		for(let i = 0; i < deletedBudget.budgetItem.length; i++){
-			deletedBudgetItemsIds.push(deletedBudget.budgetItem[i].id)
-		}
-		const deletedBudgetItems = await BudgetItems.deleteMany({
-			_id: {$in: deletedBudgetItemsIds}
-		})
+		const foundUser = await User.findOne({username: req.session.username})
+		console.log(foundUser, ' First console log of user');
+		console.log(req.params.id, ' These are the params');
+		const deletedBudget = await Budget.findOneAndRemove({_id: req.params.id});
+		console.log(deletedBudget, ' Deleted budget console log');
+		foundUser.budget.splice(foundUser.budget.findIndex((budget) => {
+			return budget.id === deletedBudget.id
+		}), 1)
+		// let deletedBudgetItemsIds = [];
+		// for(let i = 0; i < deletedBudget.budgetItem.length; i++){
+		// 	deletedBudgetItemsIds.push(deletedBudget.budgetItem[i].id)
+		// }
+		// const deletedBudgetItems = await BudgetItems.deleteMany({
+		// 	_id: {$in: deletedBudgetItemsIds}
+		// })
+		console.log(foundUser, ' Second console log of user');
 		res.json({
 			status: 200,
-			data: deletedBudget
+			data: foundUser
 		})
 	} catch(err) {
 		res.send(err)
