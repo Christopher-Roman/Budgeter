343becd2108f8d3c5b4dc7666568216c509e945e
Chris Roman <Christopherroman85@gmail.com>
2019-06-14 14:29:05 -0500

    setting up session store

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser 	 = require('body-parser');
 const methodOverride = require('method-override')
 const session 		 = require('express-session');
 const cors 			 = require('cors');
+const AerospikeStore = require('aerospike-session-store')(session)
 require('dotenv').config({path:'./.env'})
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
@@ -20,6 +21,12 @@ require('./db/db')
 
 app.use(session({
 	secret: process.env.SECRET,
+	store: new AerospikeStore({
+    namespace: 'express',
+    set: 'session',
+    ttl: 86400, // 1 day
+    hosts: process.env.HOST
+  }),
 	resave: 'false',
 	saveUninitialized: false
 }));
